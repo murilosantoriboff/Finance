565057ad3ca061151c416ec909075be0c1a78181
Murilo Santori Boff <murilosantori45@gmail.com>
2023-07-08 07:24:23 -0300

    Db, and signing banks

--- perfil/views.py
+++ perfil/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def gerenciar(request):
+    return render(request, 'gerenciar.html')
+
+def cadastrar_banco(request):
+    apelido = request.POST.get('apelido')
+    banco = request.POST.get('banco')
+    tipo = request.POST.get('tipo')
+    valor = request.POST.get('valor')
+    icone = request.FILES.get('icone')
+
+    
