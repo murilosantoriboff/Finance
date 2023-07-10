4cc1852c212a41965064caf048438ab09a916b6c
Murilo Santori Boff <murilosantori45@gmail.com>
2023-07-10 20:41:09 -0300

    Messages of success and fail creation of account

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from django.contrib.messages import constants
 from pathlib import Path
 import os
 
@@ -131,3 +131,11 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info ',
+}
--- perfil/views.py
+++ perfil/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Conta, Categoria
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html')
 
 def gerenciar(request):
-    return render(request, 'gerenciar.html')
+    contas = Conta.objects.all()
+    return render(request, 'gerenciar.html', {'contas':contas})
 
 def cadastrar_banco(request):
     apelido = request.POST.get('apelido')
@@ -12,5 +16,10 @@ def cadastrar_banco(request):
     tipo = request.POST.get('tipo')
     valor = request.POST.get('valor')
     icone = request.FILES.get('icone')
-
-    
+    if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
+        messages.add_message(request, constants.ERROR, 'Não existem dados o suficiente no formulario')
+        return redirect('/perfil/gerenciar/')
+    conta = Conta(apelido=apelido, banco=banco, tipo=tipo, valor=valor, icone=icone)
+    conta.save()
+    messages.add_message(request, constants.SUCCESS, 'Conta criada com sucesso')
+    return redirect('/perfil/gerenciar/')
