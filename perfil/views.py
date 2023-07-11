dffdbcd3c2a3c9e6148e5f43fbc5c2ff46e89f77
Murilo Santori Boff <murilosantori45@gmail.com>
2023-07-11 20:32:11 -0300

    Updating views and create functions

--- perfil/views.py
+++ perfil/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render, redirect
 from .models import Conta, Categoria
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.http import HttpResponse
+from .utils import calcula_total
 
 def home(request):
-    return render(request, 'home.html')
+    contas = Conta.objects.all()
+    total = calcula_total(contas, 'valor')
+    return render(request, 'home.html', {'contas':contas, 'total': total})
 
 def gerenciar(request):
     contas = Conta.objects.all()
-    return render(request, 'gerenciar.html', {'contas':contas})
+    categorias = Categoria.objects.all()
+    total = calcula_total(contas, 'valor')
+    return render(request, 'gerenciar.html', {'contas':contas, 'total_conta': total, 'categorias':categorias})
 
 def cadastrar_banco(request):
     apelido = request.POST.get('apelido')
@@ -22,4 +28,38 @@ def cadastrar_banco(request):
     conta = Conta(apelido=apelido, banco=banco, tipo=tipo, valor=valor, icone=icone)
     conta.save()
     messages.add_message(request, constants.SUCCESS, 'Conta criada com sucesso')
+    return redirect('/perfil/gerenciar/')
+
+def deletar_banco(request, id):
+    conta = Conta.objects.get(id=id)
+    conta.delete()
+
+    messages.add_message(request, constants.SUCCESS, 'Conta Removida com sucesso')
+    return redirect('/perfil/gerenciar/')
+
+def cadastrar_categoria(request):
+    nome = request.POST.get('categoria')
+    essencial = bool(request.POST.get('essencial'))
+
+    if len(nome.strip()) == 0:
+        messages.add_message(request, constants.ERROR, 'Não existem dados o suficiente no formulario')
+        return redirect('/perfil/gerenciar/')
+    if isinstance(essencial, bool) == True:
+        categoria = Categoria(
+            categoria=nome,
+            essencial=essencial
+        )
+
+        categoria.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Categoria cadastrada com sucesso')
+        return redirect('/perfil/gerenciar/')
+    else:
+        messages.add_message(request, constants.ERROR, 'Não existem dados o suficiente no formulario')
+        return redirect('/perfil/gerenciar/')
+    
+def update_categoria(request, id):
+    categoria = Categoria.objects.get(id=id)
+    categoria.essencial = not categoria.essencial
+    categoria.save()
     return redirect('/perfil/gerenciar/')
